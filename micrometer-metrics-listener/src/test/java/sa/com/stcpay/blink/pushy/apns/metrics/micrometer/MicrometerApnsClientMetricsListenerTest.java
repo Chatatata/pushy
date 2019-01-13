95539ac50bca5ee694c2e8196bc625647ba61c20
Buğra Ekuklu <ekuklu@icloud.com>
2019-01-13 22:15:32 +0300

    Change package names to STCPay namespace.

---
+++ micrometer-metrics-listener/src/test/java/sa/com/stcpay/blink/pushy/apns/metrics/micrometer/MicrometerApnsClientMetricsListenerTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2013-2018 Turo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package sa.com.stcpay.blink.pushy.apns.metrics.micrometer;
+
+import sa.com.stcpay.blink.pushy.util.SimpleApnsPushNotification;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MicrometerApnsClientMetricsListenerTest {
+
+    protected static final String TOKEN = "token";
+    protected static final String TOPIC = "topic";
+    protected static final String PAYLOAD = "payload";
+    protected static final String REJECTION_REASON = "rejection reason";
+
+    private MeterRegistry meterRegistry;
+    private MicrometerApnsClientMetricsListener listener;
+    private SimpleApnsPushNotification notification;
+
+    @Before
+    public void setUp() {
+        this.meterRegistry = new SimpleMeterRegistry();
+        this.listener = new MicrometerApnsClientMetricsListener<SimpleApnsPushNotification>(this.meterRegistry);
+        this.notification = new SimpleApnsPushNotification(TOKEN, TOPIC, PAYLOAD);
+    }
+
+    @Test
+    public void testHandleWriteFailure() {
+        final Counter writeFailures = this.meterRegistry.get(MicrometerApnsClientMetricsListener.WRITE_FAILURES_COUNTER_NAME).counter();
+        assertEquals(0, (int) writeFailures.count());
+
+        this.listener.handleWriteFailure(null, notification);
+        assertEquals(1, (int) writeFailures.count());
+    }
+
+    @Test
+    public void testHandleNotificationSent() {
+        final Counter sentNotifications = this.meterRegistry.get(MicrometerApnsClientMetricsListener.SENT_NOTIFICATIONS_COUNTER_NAME).counter();
+        assertEquals(0, (int) sentNotifications.count());
+
+        this.listener.handleNotificationSent(null, notification);
+        assertEquals(1, (int) sentNotifications.count());
+    }
+
+    @Test
+    public void testHandleNotificationAccepted() {
+        final Counter acceptedNotifications = this.meterRegistry.get(MicrometerApnsClientMetricsListener.ACCEPTED_NOTIFICATIONS_COUNTER_NAME).counter();
+        assertEquals(0, (int) acceptedNotifications.count());
+
+        this.listener.handleNotificationAccepted(null, notification);
+        assertEquals(1, (int) acceptedNotifications.count());
+    }
+
+    @Test
+    public void testHandleNotificationRejected() {
+        final Counter rejectedNotifications = this.meterRegistry.get(MicrometerApnsClientMetricsListener.REJECTED_NOTIFICATIONS_COUNTER_NAME).counter();
+        assertEquals(0, (int) rejectedNotifications.count());
+
+        this.listener.handleNotificationRejected(null, notification, REJECTION_REASON);
+        assertEquals(1, (int) rejectedNotifications.count());
+    }
+
+    @Test
+    public void testHandleConnectionAddedAndRemoved() {
+        final Gauge openConnectionGauge = this.meterRegistry.get(MicrometerApnsClientMetricsListener.OPEN_CONNECTIONS_GAUGE_NAME).gauge();
+
+        this.listener.handleConnectionAdded(null);
+
+        assertEquals(1, (int) openConnectionGauge.value());
+
+        this.listener.handleConnectionRemoved(null);
+
+        assertEquals(0, (int) openConnectionGauge.value());
+    }
+
+    @Test
+    public void testHandleConnectionCreationFailed() {
+        final Counter connectionFailures = this.meterRegistry.get(MicrometerApnsClientMetricsListener.CONNECTION_FAILURES_COUNTER_NAME).counter();
+        assertEquals(0, (int) connectionFailures.count());
+
+        this.listener.handleConnectionCreationFailed(null);
+        assertEquals(1, (int) connectionFailures.count());
+    }
+}
